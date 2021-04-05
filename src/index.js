eedb3c581dce4a2d815723db0b52874f2e10a957
Holovatiuk Victor <Stethem696@gmail.com>
2021-04-06 01:44:16 +0300

    Сделан таймер

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+import './timer.js';
 import './styles.css';
---
+++ src/timer.js
@@ -0,0 +1,44 @@
+// const date1 = Date.now();
+// console.log('date1', date1);
+// // 1 января 1970 00: 00;
+
+// // разница во времени
+
+// setTimeout(() => {
+//   const date2 = Date.now();
+//   console.log('date1', date1);
+//   console.log('date2', date2);
+
+//   console.log(date2 - date1);
+// }, 3000);
+
+// -------------------------------------------------
+const timer = {
+  start() {
+    const startTime = Date.now();
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+      console.log(`${days}:${hours}:${mins}:${secs}`);
+    }, 1000);
+  },
+};
+
+timer.start();
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}
